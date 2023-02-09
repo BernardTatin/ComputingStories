b458d116588a673e8ba3be00a866b51473b992bf
Bernard Tatin <btat1.ernard@tutanota.com>
2023-02-09 21:10:16 +0100

    PackCC-calc: a big failure...

--- lib/include/int128.h
+++ lib/include/int128.h
@@ -21,16 +21,33 @@
     typedef __int128 Cint;
 
     static const Cint i128_max = ((Cint)INT64_MAX << 64) | ((Cint)UINT64_MAX);
+    static const Cint i128_carry_bit = 1 << 127;
+
     CONS_FUNC char *i128_to_char(const Cint);
 
     static inline bool s_add(const Cint a, const Cint b, Cint *r) {
         bool result = true;
 
+    #if 0
+        // a big failure! Ouch!
+        Cint s = (a ^ i128_carry_bit) + (b ^ i128_carry_bit);
+        bool c = (s & i128_carry_bit) !=0;
+        bool all_pos = (a > 0) && (b > 0);
+        bool all_neg = (a < 0) && (b < 0);
+        bool overflow = (c && all_pos) || (!c && all_neg);
+        if (!overflow) {
+            *r = s;
+        } else {
+            result = false;
+        }
+
+    #else
         if (a < i128_max - b) {
             *r = a + b;
         } else {
             result = false;
         }
+    #endif
         return result;
     }
 #endif
